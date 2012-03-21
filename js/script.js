c8f3725567a0c601b82f680060c31c50d2423b6d
Jarrod Overson <joverson@onerecovery.com>
2012-03-21 00:13:41 -0700

    adding interactive JS

--- js/script.js
+++ js/script.js
@@ -2,8 +2,29 @@
 
 */
 
+var editor;
 
-$('#content, header, footer').click(function(){
-  $('header, footer, #content').removeClass('active-face');
-  $(this).addClass('active-face');
-})
+$(function(){
+
+  $('#expose-playground').click(exposePlayground);
+  $('#execute-playground').click(executePlayground);
+
+  editor = CodeMirror.fromTextArea(document.getElementById("javascript-playground"), {
+    mode: "text/javascript"
+  });
+
+});
+
+function exposePlayground() {
+  $(this).fadeOut(function(){$('#execute-playground').fadeIn()});
+  $('#playground-container').animate({height:'100px'});
+}
+
+function executePlayground() {
+  var script = editor.getValue();
+  try {
+    eval(script);
+  } catch(e) {
+    if (console) console.log(e.toString());
+  }
+}
