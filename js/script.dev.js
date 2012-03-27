e12a633ecc38ce3a98f57d1cec076b55a6c17f0a
Jarrod Overson <joverson@onerecovery.com>
2012-03-26 21:44:56 -0700

    backup

---
+++ js/script.dev.js
@@ -0,0 +1,44 @@
+/* Author:
+
+*/
+
+var editor,evalError;
+
+$(function(){
+
+  $('#expose-playground').click(exposePlayground);
+  $('#execute-playground').click(executePlayground);
+
+  editor = CodeMirror.fromTextArea(document.getElementById("javascript-playground"), {
+    mode: "text/javascript"
+  });
+
+  //setup error popup
+  $('.CodeMirror').popover({
+    title     : 'Oops!',
+    content   : showError,
+    trigger   : 'manual',
+    placement : 'bottom'
+  })
+});
+
+function exposePlayground() {
+  $(this).fadeOut(250,function(){$('#execute-playground').fadeIn(250)});
+  $('#playground-container').animate({height:'100px'});
+}
+
+function showError() {
+  return evalError;
+}
+
+function executePlayground() {
+  var script = editor.getValue();
+  try {
+    eval(script);
+    $('.CodeMirror').popover('hide');
+  } catch(e) {
+    evalError = e.toString();
+    $('.CodeMirror').popover('show');
+    if (console) console.log(e.toString());
+  }
+}
